fc4ca41aeca620fd57a9d7a5f99f73ff3f79fa3b
nikitakaralius <nikitakaralyus@gmail.com>
2022-06-25 00:21:24 +0400

    Added Triangle Area calculation

--- src/Planimetry/Shapes/Triangle.cs
+++ src/Planimetry/Shapes/Triangle.cs
@@ -1,9 +1,31 @@
 ﻿using Planimetry.Core;
+using Planimetry.Exceptions;
+using static System.Math;
 
 namespace Planimetry.Shapes
 {
     public class Triangle : IShape
     {
-        public double Area { get; }
+        public Triangle(double a, double b, double c)
+        {
+            ShapeParameterException.ThrowIfNegative(a, nameof(a));
+            ShapeParameterException.ThrowIfNegative(c, nameof(b));
+            ShapeParameterException.ThrowIfNegative(c, nameof(c));
+            (A, B, C) = (a, b, c);
+        }
+
+        public double A { get; }
+
+        public double B { get; }
+
+        public double C { get; }
+
+        public double Area => AreaByHeronsFormula();
+
+        private double AreaByHeronsFormula()
+        {
+            double semiPerimeter = (A + B + C) / 2;
+            return Sqrt(semiPerimeter * (semiPerimeter - A) * (semiPerimeter - B) * (semiPerimeter - C));
+        }
     }
 }
