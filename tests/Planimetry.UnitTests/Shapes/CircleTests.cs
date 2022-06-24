5a54497df3afc1ee4ca449785eb3333a011aa0a4
nikitakaralius <nikitakaralyus@gmail.com>
2022-06-25 00:40:12 +0400

    Added Circle shape tests

--- tests/Planimetry.UnitTests/Shapes/CircleTests.cs
+++ tests/Planimetry.UnitTests/Shapes/CircleTests.cs
@@ -3,5 +3,18 @@
 [TestFixture]
 public class CircleTests
 {
-    
+    [Test]
+    public void CalculatesAreaByRadius()
+    {
+        double radius = 123.456;
+        Circle circle = new(radius);
+        circle.Area.Should().Be(PI * radius * radius);
+    }
+
+    [Test]
+    public void ThrowsExceptionIfRadiusIsNegative()
+    {
+        Action initialization = () => new Circle(-1.12);
+        initialization.Should().Throw<ShapeParameterException>();
+    }
 }
