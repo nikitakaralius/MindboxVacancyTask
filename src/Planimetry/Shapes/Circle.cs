ca8bf6b7c7c22ec25106d5c61ada23327f6b175d
nikitakaralius <nikitakaralyus@gmail.com>
2022-06-25 00:16:01 +0400

    Created Circle shape

--- src/Planimetry/Shapes/Circle.cs
+++ src/Planimetry/Shapes/Circle.cs
@@ -1,9 +1,19 @@
 ﻿using Planimetry.Core;
+using Planimetry.Exceptions;
+using static System.Math;
 
 namespace Planimetry.Shapes
 {
     public class Circle : IShape
     {
-        public double Area { get; }
+        public Circle(double radius)
+        {
+            ShapeParameterException.ThrowIfNegative(radius, nameof(radius));
+            Radius = radius;
+        }
+
+        public double Radius { get; }
+
+        public double Area => PI * Radius * Radius;
     }
 }
