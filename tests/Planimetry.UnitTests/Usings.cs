12aa6fbe2f09b126b4bc16f7c21ed406b48a8b82
nikitakaralius <nikitakaralyus@gmail.com>
2022-06-25 00:44:27 +0400

    Craeted Shuffled List Extension Method in order to create Triangle tests

--- tests/Planimetry.UnitTests/Usings.cs
+++ tests/Planimetry.UnitTests/Usings.cs
@@ -2,4 +2,5 @@ global using NUnit.Framework;
 global using FluentAssertions;
 global using Planimetry.Shapes;
 global using Planimetry.Exceptions;
+global using Planimetry.UnitTests.Extensions;
 global using static System.Math;
