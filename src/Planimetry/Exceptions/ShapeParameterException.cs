1a3c1d12c80c3f5a1e9011ee9e5e8a13b9598832
nikitakaralius <nikitakaralyus@gmail.com>
2022-06-25 00:08:28 +0400

    Added ShapeParamaterException

---
+++ src/Planimetry/Exceptions/ShapeParameterException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Planimetry.Exceptions
+{
+    public class ShapeParameterException : ArgumentException
+    {
+        public ShapeParameterException(string message, string paramName) : base(message, paramName) { }
+
+        public static void ThrowIfNegative(double value, string paramName)
+        {
+            if (value < 0)
+            {
+                throw new ShapeParameterException($"{paramName} can not be negative", paramName);
+            }
+        }
+    }
+}
