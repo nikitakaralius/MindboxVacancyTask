4560a9aec400badb2ca2e5234fa3658e047cea87
nikitakaralyus <nikitakaralyus@gmail.com>
2022-09-19 18:34:36 +0300

    Made shape constructors private

--- src/Planimetry/Shapes/Triangle.cs
+++ src/Planimetry/Shapes/Triangle.cs
@@ -7,19 +7,9 @@ using static System.Array;
 
 namespace Planimetry.Shapes
 {
-    public class Triangle : IShape
+    public class Triangle : IShape, IEquatable<Triangle>
     {
-        public Triangle(double a, double b, double c)
-        {
-            ShapeParameterException.ThrowIfNegative(a, nameof(a));
-            ShapeParameterException.ThrowIfNegative(b, nameof(b));
-            ShapeParameterException.ThrowIfNegative(c, nameof(c));
-            if (TriangleExists(a, b, c) == false)
-            {
-                throw new TriangleInequalityException(a, b, c);
-            }
-            (A, B, C) = (a, b, c);
-        }
+        private Triangle(double a, double b, double c) => (A, B, C) = (a, b, c);
 
         public double A { get; }
 
@@ -36,6 +26,7 @@ namespace Planimetry.Shapes
                     double semiPerimeter = (A + B + C) / 2;
                     return Sqrt(semiPerimeter * (semiPerimeter - A) * (semiPerimeter - B) * (semiPerimeter - C));
                 }
+
                 return HeronsFormula();
             }
         }
@@ -49,24 +40,40 @@ namespace Planimetry.Shapes
                     double[] sides = SidesInAscendingOrder();
                     return Abs(sides[2] * sides[2] - (sides[0] * sides[0] + sides[1] * sides[1])) < 1E-5;
                 }
+
                 return InversePythagoreanTheorem();
             }
         }
 
-        public static Triangle From(IReadOnlyList<double> triplet)
+        public static Triangle Create(double a, double b, double c)
+        {
+            ShapeParameterException.ThrowIfNegative(a, nameof(a));
+            ShapeParameterException.ThrowIfNegative(b, nameof(b));
+            ShapeParameterException.ThrowIfNegative(c, nameof(c));
+
+            if (TriangleExists(a, b, c) == false)
+            {
+                throw new TriangleInequalityException(a, b, c);
+            }
+
+            return new Triangle(a, b, c);
+        }
+
+        public static Triangle Create(IReadOnlyList<double> triplet)
         {
             if (triplet.Count != 3)
             {
                 throw new ArgumentException("Triplet must contain exactly 3 elements", nameof(triplet));
             }
-            return new Triangle(triplet[0], triplet[1], triplet[2]);
+
+            return Create(triplet[0], triplet[1], triplet[2]);
         }
 
         public double[] SidesInAscendingOrder() => SidesInAscendingOrder(A, B, C);
 
         private static double[] SidesInAscendingOrder(double a, double b, double c)
         {
-            double[] sides = { a, b, c };
+            double[] sides = {a, b, c};
             Sort(sides);
             return sides;
         }
@@ -75,6 +82,26 @@ namespace Planimetry.Shapes
         {
             double[] sides = SidesInAscendingOrder(a, b, c);
             return sides[2] < sides[0] + sides[1];
+        }
+
+        public bool Equals(Triangle? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return A.Equals(other.A) && B.Equals(other.B) && C.Equals(other.C);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Triangle) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(A, B, C);
         }
     }
 }
--- tests/Planimetry.UnitTests/Shapes/CircleTests.cs
+++ tests/Planimetry.UnitTests/Shapes/CircleTests.cs
@@ -7,14 +7,14 @@ public class CircleTests
     public void CalculatesAreaByRadius()
     {
         double radius = 123.456;
-        Circle circle = new(radius);
+        Circle circle = Circle.Create(radius);
         circle.Area.Should().Be(PI * radius * radius);
     }
 
     [Test]
     public void ThrowsExceptionIfRadiusIsNegative()
     {
-        Action initialization = () => new Circle(-1.12);
+        Action initialization = () => Circle.Create(-1.12);
         initialization.Should().Throw<ShapeParameterException>();
     }
 }
